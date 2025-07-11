8581635ea9ddc35f87d9e6e796b717de3501f576
Damiano Schirinzi <damianoschirinzi.dev@gmail.com>
2025-07-11 12:16:07 +0100

    resolving build error with Rule type

--- sanity/schemas/coach.ts
+++ sanity/schemas/coach.ts
@@ -1,24 +1,26 @@
+import { Rule } from "sanity";
+
 export default {
-  name: 'coach',
-  title: 'Coach',
-  type: 'document',
+  name: "coach",
+  title: "Coach",
+  type: "document",
   fields: [
-    { name: 'name', title: 'Name', type: 'string' },
+    { name: "name", title: "Name", type: "string" },
     {
-      name: 'order',
-      title: 'Display Order',
-      type: 'number',
-      validation: Rule => Rule.required(),
+      name: "order",
+      title: "Display Order",
+      type: "number",
+      validation: (Rule: Rule) => Rule.required(),
     },
-    { name: 'role', title: 'Role', type: 'string' },
-    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'name' } },
-    { name: 'bio', title: 'Bio', type: 'text' },
-    { name: 'image', title: 'Image', type: 'image' },
+    { name: "role", title: "Role", type: "string" },
+    { name: "slug", title: "Slug", type: "slug", options: { source: "name" } },
+    { name: "bio", title: "Bio", type: "text" },
+    { name: "image", title: "Image", type: "image" },
     {
-      name: 'specialties',
-      title: 'Specialties',
-      type: 'array',
-      of: [{ type: 'string' }],
+      name: "specialties",
+      title: "Specialties",
+      type: "array",
+      of: [{ type: "string" }],
     },
   ],
-}
+};
